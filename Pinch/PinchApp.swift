3b2441dc7fc741129432abc434dd5fd7dc6feee7
Vlad <160039489+55kt@users.noreply.github.com>
2024-12-04 20:09:39 +0200

    First commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Vlad on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
