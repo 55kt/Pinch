d512dfe45ad833ede40a4bb8320d275effef515d
Vlad <160039489+55kt@users.noreply.github.com>
2024-12-06 20:31:53 +0200

    Long press gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -26,6 +26,8 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
+                
                 // MARK: - Page Image
                 Image("magazine-front-cover")
                     .resizable()
@@ -68,6 +70,13 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
+            // MARK: - Info Panel
+            .overlay (
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                ,alignment: .top
+            )
         } //: Navigation
         .navigationViewStyle(.stack)
     }
